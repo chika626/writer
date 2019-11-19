76c1569e6a1f2ec4841783bc84bd3f4de02a7015
chika626 <21084340+chika626@users.noreply.github.com>
2019-11-19 19:00:14 +0900

    #FirstCommit

---
+++ Converter/Console_Worker/Functions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Security.Principal;
+
+
+namespace Console_Worker
+{
+    public class Func
+    {
+        //最初に権限を取っ払う
+        public static void FirstRun(string full_file_pas)
+        {
+            GrantAuthority(full_file_pas);
+            EraseReadOnly(full_file_pas);
+        }
+
+        //アクセス権限付与
+        //書き込み不能ファイルをなくす
+        //フルパス+ファイル名含む
+        public static void GrantAuthority(string file_pas)
+        {
+            FileSystemAccessRule rule = new FileSystemAccessRule(
+                  new NTAccount("everyone"),
+                  FileSystemRights.Modify,
+                  AccessControlType.Allow);
+            FileSecurity security = File.GetAccessControl(file_pas);
+            security.AddAccessRule(rule);
+            File.SetAccessControl(file_pas, security);
+        }
+
+
+        //読み取り専用の場合消す
+        //フルパス+ファイル名含む
+        public static void EraseReadOnly(string file_pas)
+        {
+            //file_pasの属性を取得する
+            System.IO.FileAttributes attr = System.IO.File.GetAttributes(file_pas);
+            //読み取り専用属性があるか調べる
+            if ((attr & System.IO.FileAttributes.ReadOnly) ==
+                System.IO.FileAttributes.ReadOnly)
+            {
+                //読み取り専用属性を削除する
+                System.IO.File.SetAttributes(file_pas,
+                    attr & (~System.IO.FileAttributes.ReadOnly));
+            }
+        }
+
+
+        //隠しファイルの場合True
+        //フルパス+ファイル名含む
+        public static bool HiddenJudge(string full_file_pas)
+        {
+            return ((new System.IO.FileInfo(full_file_pas).Attributes &
+                System.IO.FileAttributes.Hidden) ==
+                System.IO.FileAttributes.Hidden ? true : false);
+        }
+
+        //特定の拡張子ファイルを探して返す
+        //( FolderPas , 拡張子[] )
+        public static string[] GetFiles(string path, params string[] extensions)
+        {
+            return Directory
+                .GetFiles(path, "*.*")
+                .Where(c => extensions.Any(extension => c.EndsWith(extension)))
+                .ToArray();
+        }
+    }
+}
---
+++ Converter/Console_Worker/Prog.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static Input;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Worker
+{
+    class Prog
+    {
+        public void Main()
+        {
+            List<Work> worker = new List<Work>();
+            //開始時に作成用データを受け取る
+            //終了文字(path == "-1")まで読む
+            while (true)
+            {
+                string path = NextString();
+                //filestream
+                if (path == "-1") break;
+                int pages = NextInt();
+                bool write = (NextString() == "1");
+                bool add = (NextString() == "1");
+                Work work = new Work(path, pages, write, add);
+                worker.Add(work);
+            }
+
+            //work
+            foreach (var e in worker)
+            {
+                e.Write();
+            }
+
+            //integ
+
+
+            Console.WriteLine("Complated");
+            return;
+        }
+
+        //all integrall func
+    }
+    class Work
+    {
+        private string fullpath;
+        private int pages;
+        private bool write;
+        private bool addition;
+
+        public Work(string path, int p, bool w, bool a)
+        {
+            fullpath = path;
+            pages = p;
+            write = w;
+            addition = a;
+        }
+
+        //write func
+        public async void Write()
+        {
+            if (!Confirm())
+            {
+                //失敗
+                ErrorLog("erroe : 隠しファイル");
+            }
+
+            //書き込む
+
+        }
+
+        private bool Confirm()
+        {
+            //隠しファイル判定
+            if (Func.HiddenJudge(fullpath)) return false;
+            Func.FirstRun(fullpath);
+            return true;
+        }
+
+
+        private void ErrorLog(string s)
+        {
+            Console.WriteLine(s);
+        }
+    }
+
+}
+
+public class Input
+{
+    public const long MOD = 1000000007;
+    public const int INF = 1000000007;
+    private static Queue<string> q = new Queue<string>();
+    private static void Confirm() { if (q.Count == 0) foreach (var s in Console.ReadLine().Split(' ')) q.Enqueue(s); }
+    public static int NextInt() { Confirm(); return int.Parse(q.Dequeue()); }
+    public static long NextLong() { Confirm(); return long.Parse(q.Dequeue()); }
+    public static string NextString() { Confirm(); return q.Dequeue(); }
+    public static double NextDouble() { Confirm(); return double.Parse(q.Dequeue()); }
+    public static int[] LineInt() { return Console.ReadLine().Split(' ').Select(int.Parse).ToArray(); }
+    public static long[] LineLong() { return Console.ReadLine().Split(' ').Select(long.Parse).ToArray(); }
+    public static string[] LineString() { return Console.ReadLine().Split(' ').ToArray(); }
+    public static double[] LineDouble() { return Console.ReadLine().Split(' ').Select(double.Parse).ToArray(); }
+}
